c57c58413700ed4e20d057c9e6f36cf92c3fc454
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-09-20 15:43:16 -0700

    commit tree 84c107bb1086aaea2e0c474736a0476b12a92b0b

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,14 @@
 class BirdsController < ApplicationController
 
+  def increment_likes
+    # find the bird we're trying to update
+    bird = Bird.find_by(id: params[:id])
+    # update the bird, use the number of current likes + 1 to determine the next number of likes
+    bird.update(likes: bird.likes + 1)
+    # send a response with the updated bird
+    render json: bird
+  end
+
   # GET /birds
   def index
     birds = Bird.all
@@ -22,10 +31,21 @@ class BirdsController < ApplicationController
     end
   end
 
+  # PATCH /birds/:id
+  def update 
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else 
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
